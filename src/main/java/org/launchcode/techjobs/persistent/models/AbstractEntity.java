7eab42c33f0d855c6d430dd115c26f544b89d313
June Lee <junelee@Junes-Laptop.local>
2024-06-14 21:44:04 -0500

    test 1 fails, working on task2 for now

--- src/main/java/org/launchcode/techjobs/persistent/models/AbstractEntity.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/AbstractEntity.java
@@ -8,7 +8,7 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 import java.util.Objects;
-
+@MappedSuperclass
 public abstract class AbstractEntity {
 
     private int id;
